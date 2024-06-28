02dd91f0fcad7781956667a3ba11a0a2f210c460
matheusgonzalez91 <gonzalezmatheus01@gmail.com>
2024-06-28 14:29:03 -0300

    codepipeline update/deploy job

--- lib/my-pipeline-stack.ts
+++ lib/my-pipeline-stack.ts
@@ -15,9 +15,15 @@ export class MyPipelineStack extends cdk.Stack {
       removalPolicy: cdk.RemovalPolicy.DESTROY, // Remover o bucket quando o stack for destruído
     });
 
+    // Bucket S3 para armazenar scripts de ETL do Glue
+    const glueScriptsBucket = new s3.Bucket(this, 'GlueScriptsBucket', {
+      removalPolicy: cdk.RemovalPolicy.RETAIN, // Manter o bucket quando o stack for destruído
+    });
+
     // Definir as variáveis de entrada do pipeline
     const sourceOutput = new codepipeline.Artifact();
     const cdkBuildOutput = new codepipeline.Artifact();
+    const glueScriptOutput = new codepipeline.Artifact();
 
     // Role do IAM para o CodePipeline acessar o Secrets Manager
     const pipelineRole = new iam.Role(this, 'PipelineRole', {
@@ -33,7 +39,7 @@ export class MyPipelineStack extends cdk.Stack {
       ],
     }));
 
-    // Definir a ação de origem (por exemplo, GitHub)
+    // Definir a ação de origem
     const sourceAction = new codepipeline_actions.GitHubSourceAction({
       actionName: 'GitHub_Source',
       owner: 'matheusgonzalez91',
@@ -43,7 +49,7 @@ export class MyPipelineStack extends cdk.Stack {
       branch: 'main',
     });
 
-    // Definir o projeto de build do CodeBuild
+    // Definir o projeto de build do CodeBuild para o CDK
     const cdkBuildProject = new codebuild.PipelineProject(this, 'CdkBuildProject', {
       buildSpec: codebuild.BuildSpec.fromObject({
         version: '0.2',
@@ -72,12 +78,48 @@ export class MyPipelineStack extends cdk.Stack {
       },
     });
 
-    // Definir a ação de build
+    // Definir o projeto de build do CodeBuild para os scripts do Glue
+    const glueScriptBuildProject = new codebuild.PipelineProject(this, 'GlueScriptBuildProject', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          install: {
+            commands: [
+              'pip install awscli', // Instalar AWS CLI para fazer o upload dos scripts
+            ],
+          },
+          build: {
+            commands: [
+              'aws s3 cp my_glue_jobs/ s3://myprontuariobucket/results/ --recursive', // Upload dos scripts para o bucket S3
+            ],
+          },
+        },
+        artifacts: {
+          'base-directory': 'my_glue_jobs',
+          files: [
+            '**/*',
+          ],
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+      },
+    });
+
+    // Definir a ação de build para o CDK
     const buildAction = new codepipeline_actions.CodeBuildAction({
       actionName: 'CDK_Build',
       project: cdkBuildProject,
       input: sourceOutput,
       outputs: [cdkBuildOutput],
+    });
+
+    // Definir a ação de build para os scripts do Glue
+    const glueScriptBuildAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'Glue_Script_Build',
+      project: glueScriptBuildProject,
+      input: sourceOutput,
+      outputs: [glueScriptOutput],
     });
 
     // Definir a ação de deploy do CloudFormation
@@ -86,6 +128,30 @@ export class MyPipelineStack extends cdk.Stack {
       stackName: 'MyAthenaUserStack',
       templatePath: cdkBuildOutput.atPath('MyAthenaUserStack.template.json'),
       adminPermissions: true,
+    });
+
+    // Definir o projeto de build para iniciar o Glue Job
+    const glueJobStartProject = new codebuild.PipelineProject(this, 'GlueJobStartProject', {
+      buildSpec: codebuild.BuildSpec.fromObject({
+        version: '0.2',
+        phases: {
+          build: {
+            commands: [
+              'aws glue start-job-run --job-name job_prontuario'
+            ],
+          },
+        },
+      }),
+      environment: {
+        buildImage: codebuild.LinuxBuildImage.STANDARD_5_0,
+      },
+    });
+
+    // Definir a ação para iniciar o Glue Job
+    const glueJobStartAction = new codepipeline_actions.CodeBuildAction({
+      actionName: 'Start_Glue_Job',
+      project: glueJobStartProject,
+      input: glueScriptOutput,
     });
 
     // Definir o pipeline
@@ -100,11 +166,15 @@ export class MyPipelineStack extends cdk.Stack {
         },
         {
           stageName: 'Build',
-          actions: [buildAction],
+          actions: [buildAction, glueScriptBuildAction],
         },
         {
           stageName: 'Deploy',
           actions: [deployAction],
+        },
+        {
+          stageName: 'GlueJob',
+          actions: [glueJobStartAction],
         },
       ],
     });
